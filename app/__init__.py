32a8751a2b64b573dd5066742176752497e97d45
Ultramas <Ultramaster123456@gmail.com>
2025-06-22 09:51:45 -0700

    updated previous commentary

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
+from backend.config import selected_config
+from backend.api.extentions import db
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
-
+    CORS(app)
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
@@ -21,4 +25,7 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
+    if app.app_context():
+        from backend.api.database.models import (SpottedPiShot, SpottedPetTicket, LostPet, LostPetImage, User,
+                                                 SpottedPetShot)
     return app
